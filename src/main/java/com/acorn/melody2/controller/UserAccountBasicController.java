814d666b95c1618a59131aa18e99018ddf3fc241
gujongkim <rnwhd77@naver.com>
2023-10-26 13:40:49 +0900

    Revert "Revert "Revert "리버트를 위한 커밋"""
    This reverts commit 4c259deb713d44a645e3185ae0de6a66f8cd791f.

--- src/main/java/com/acorn/melody2/controller/UserAccountBasicController.java
+++ src/main/java/com/acorn/melody2/controller/UserAccountBasicController.java
@@ -35,6 +35,13 @@ public class UserAccountBasicController {
     }
 
     @PostMapping
+    public ResponseEntity<UserAccount> createUserAccount(@RequestBody UserAccount userAccount) {
+        //age group 계산해서 추가
+        String ageGroup = calculateAgeGroup(userAccount.getBirthDate());
+        userAccount.setAgeGroup(ageGroup);
+        UserAccount createdUserAccount = userAccountService.createUserAccount(userAccount);
+        return new ResponseEntity<>(createdUserAccount, HttpStatus.CREATED);
+    }
 
     @PutMapping("/{id}")
     public ResponseEntity<UserAccount> updateUserAccount(@PathVariable Long id, @RequestBody UserAccount updatedUserAccount) {
